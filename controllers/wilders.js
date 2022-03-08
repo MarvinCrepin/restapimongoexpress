3b91fea42c25357d19c6dda8e963bbc1362c7c6c
MarvinCrepin <marvincrepin@gmail.com>
2022-03-08 16:15:08 +0100

    Comment and EN => FR

--- controllers/wilders.js
+++ controllers/wilders.js
@@ -21,7 +21,7 @@ module.exports = {
   update: (req, res) => {
     const options = { new: true };
     // On utilise la méthode findByIdAndDelete afin de retrouver le document et le supprimer
-    // On lui passe en paramètre l'id, le payload (les éléments du document à mettre à jour)
+    // On lui passe en paramètre l'id, le payload (les éléments du document à mettre à jour) ainsi qu'un tableau d'options optionnel
     const result = WilderModel.findByIdAndUpdate(req.params.id, req.body, options);
     result
       .then((result) => res.send(result))
